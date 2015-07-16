357870c74ae8ae27d08224ddf1ecdcc6e04d657d
xiaotie <wudixiaotie@gmail.com>
2015-07-16 13:27:51 +0800

    update

--- erlang/ssdb_pool/start.sh
+++ erlang/ssdb_pool/start.sh
@@ -1,3 +1,19 @@
 rebar compile
-# erl -pa ebin -pa deps/*/ebin -eval "observer:start ()."
-erl -pa ebin -pa deps/*/ebin -eval "application:start (ssdb_pool)." -eval "observer:start ()."
+
+case $1 in
+    "o" )
+        erl -pa ebin/ -pa deps/*/ebin -eval "observer:start ()."
+        ;;
+    "a" )
+        erl -pa ebin/ -pa deps/*/ebin -eval "application:start (ssdb_pool)."
+        ;;
+    "oa" | "ao" )
+        erl -pa ebin/ -pa deps/*/ebin -eval "observer:start ()." -eval "application:start (ssdb_pool)."
+        ;;
+    "" )
+        erl -pa ebin/ -pa deps/*/ebin
+        ;;
+    * )
+        echo "unknown args!"
+        ;;
+esac
